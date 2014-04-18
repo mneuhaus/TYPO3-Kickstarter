96c88229feb1236b5714c726bba820c768fedddb
Philipp Gampe <philipp.gampe@typo3.org>
2014-04-18 14:43:39 +0200

    [TASK] Create ext_autoload.php

---
+++ ext_autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+$path = t3lib_extMgm::extPath('kickstarter');
+
+return array(
+	'tx_kickstarter_compilefiles' => $path . 'class.tx_kickstarter_compilefiles.php',
+	'tx_kickstarter_reservedwords' => $path . 'class.tx_kickstarter_reservedwords.php',
+	'tx_kickstarter_sectionbase' => $path . 'class.tx_kickstarter_sectionbase.php',
+	'tx_kickstarter_wizard' => $path . 'class.tx_kickstarter_wizard.php',
+	'tx_kickstarter_section_cm' => $path . 'sections/class.tx_kickstarter_section_cm.php',
+	'tx_kickstarter_section_emconf' => $path . 'sections/class.tx_kickstarter_section_emconf.php',
+	'tx_kickstarter_section_fields' => $path . 'sections/class.tx_kickstarter_section_fields.php',
+	'tx_kickstarter_section_languages' => $path . 'sections/class.tx_kickstarter_section_languages.php',
+	'tx_kickstarter_section_module' => $path . 'sections/class.tx_kickstarter_section_module.php',
+	'tx_kickstarter_section_modulefunction' => $path . 'sections/class.tx_kickstarter_section_modulefunction.php',
+	'tx_kickstarter_section_pi' => $path . 'sections/class.tx_kickstarter_section_pi.php',
+	'tx_kickstarter_section_sv' => $path . 'sections/class.tx_kickstarter_section_sv.php',
+	'tx_kickstarter_section_tables' => $path . 'sections/class.tx_kickstarter_section_tables.php',
+	'tx_kickstarter_section_ts' => $path . 'sections/class.tx_kickstarter_section_ts.php',
+	'tx_kickstarter_section_tsconfig' => $path . 'sections/class.tx_kickstarter_section_tsconfig.php',
+);
